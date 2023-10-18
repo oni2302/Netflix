1110004190bc78adf29d947d6ed03ae379a0cd6e
NguyenDucTinNew <93468105+HuyLieu1603@users.noreply.github.com>
2023-10-18 12:50:36 +0700

    finish

--- App/Controllers/admin/Service.php
+++ App/Controllers/admin/Service.php
@@ -7,7 +7,7 @@ class Service extends AdminController
     }
     public function index(){
         $this->data['title'] = 'Quản lí gói dịch vụ';
-        $this->data['content'] = 'admin/video/index';
+        $this->data['content'] = 'admin/service/index';
         $this->data['sub_content']['service_list'] = $this->model->getAllServicePackages();
         $this->renderView('layouts/admin', $this->data);
     }
