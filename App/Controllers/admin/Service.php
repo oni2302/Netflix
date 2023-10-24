6656d05c006e7f7d1e4c18c8e143280dce873815
NguyenDucTinNew <93468105+HuyLieu1603@users.noreply.github.com>
2023-10-24 19:03:33 +0700

    sửa

--- App/Controllers/admin/Service.php
+++ App/Controllers/admin/Service.php
@@ -5,7 +5,7 @@ class Service extends AdminController
         parent::__construct();
         $this->model = $this->getModel('admin/ServiceModel');
     }
-    public function index(){
+    public function serviceIndex(){
         $this->data['title'] = 'Quản lí gói dịch vụ';
         $this->data['content'] = 'admin/service/index';
         $this->data['sub_content']['service_list'] = $this->model->getAllServicePackages();
--- Configs/routes.php
+++ Configs/routes.php
@@ -8,7 +8,7 @@
     $routes['fetch/(.+)/size'] = 'product/size/$1';
 
     //Service
-    $routes['danh-sach-voucher'] = 'admin/Service/index';
+    $routes['danh-sach-voucher'] = 'admin/Service/serviceIndex';
 
     //Tùy chỉnh đường link
     //Ví dụ $routes['duong_link'] = 'controller/action' => http://localhost/duong_link thì nó sẽ vào controller và action
