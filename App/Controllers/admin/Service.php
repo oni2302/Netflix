59b1e94031eeb3408df2d3b5a977f8946edd7a77
NguyenDucTinNew <93468105+HuyLieu1603@users.noreply.github.com>
2023-10-24 19:09:42 +0700

    sửa

--- App/Controllers/admin/Service.php
+++ App/Controllers/admin/Service.php
@@ -6,9 +6,9 @@ class Service extends AdminController
         $this->model = $this->getModel('admin/ServiceModel');
     }
     public function serviceIndex(){
-        $this->data['title'] = 'Quản lí gói dịch vụ';
         $this->data['content'] = 'admin/service/index';
         $this->data['sub_content']['service_list'] = $this->model->getAllServicePackages();
+        $this->data['title'] = 'Quản lí gói dịch vụ'; 
         $this->renderView('layouts/admin', $this->data);
     }
 }
